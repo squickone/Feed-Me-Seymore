a9f2859643e809bb69acfa38d560e2ae1c8c53f5
Whitney Powell <whitney.ellis.powell@gmail.com>
2012-01-16 14:07:15 -0500

    AddChild screen has 2 fields to add a baby, home has button to AddChild activity

--- src/main/java/com/feedme/AddChild.java
+++ src/main/java/com/feedme/AddChild.java
@@ -1,8 +1,12 @@
 package com.feedme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.List;
 
@@ -12,32 +16,46 @@ import java.util.List;
  * Time: 12:27 PM
  */
 public class AddChild extends Activity {
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.add_child);
+        final DatabaseHandler db = new DatabaseHandler(this);
+
+        // button listener for add child button
+
+        final EditText babyName = (EditText) findViewById(R.id.babyName);
+        final EditText babySex = (EditText) findViewById(R.id.babySex);
+        Button addChildButton = (Button)findViewById(R.id.addChildButton);
+
+        addChildButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                /**
+                 * CRUD Operations
+                 * */
+                // Inserting baby
+                Log.d("Insert: ", "Inserting ..");
+                db.addBaby(new Baby(babyName.getText().toString(), babySex.getText().toString()));
+
+                // Reading all babies
+                Log.d("Reading: ", "Reading all babies..");
+                List<Baby> contacts = db.getAllBabies();
+
+                for (Baby cn : contacts) {
+                    String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Sex: " + cn.getSex();
+                    // Writing babies to log
+                    Log.d("Name: ", log);
+                }
+                
+                babyName.setText("");
+                babySex.setText("");
+
+             }
+        });
 
 
-        DatabaseHandler db = new DatabaseHandler(this);
-
-        /**
-         * CRUD Operations
-         * */
-        // Inserting baby
-        Log.d("Insert: ", "Inserting ..");
-        db.addBaby(new Baby("Roger", "male"));
-
-
-        // Reading all babies
-        Log.d("Reading: ", "Reading all babies..");
-        List<Baby> contacts = db.getAllBabies();
-
-        for (Baby cn : contacts) {
-            String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Sex: " + cn.getSex();
-            // Writing babies to log
-            Log.d("Name: ", log);
-        }
     }
 }
--- src/main/java/com/feedme/Home.java
+++ src/main/java/com/feedme/Home.java
@@ -1,8 +1,11 @@
 package com.feedme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -21,6 +24,16 @@ public class Home extends Activity {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate");
         setContentView(R.layout.main);
+
+        // button listener for add child screen button
+
+        Button addChildScreen = (Button)findViewById(R.id.addChildScreen);
+        addChildScreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this,
+                        AddChild.class));
+            }
+        });
 
     }
 }
