5317d08760ea8c30ac3a8c8c41d448aa0349be4b
Whitney Powell <whitney.ellis.powell@gmail.com>
2012-01-22 16:35:40 -0500

    added validation to add/edit child to prevent force close

--- src/main/java/com/feedme/activity/AddChildActivity.java
+++ src/main/java/com/feedme/activity/AddChildActivity.java
@@ -1,9 +1,7 @@
 package com.feedme.activity;
 
-import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.app.*;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -130,42 +128,57 @@ public class AddChildActivity extends BaseActivity {
             }
         });
 
+        //declare alert dialog
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
         //Add Child Button
         addChildButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                
-                String picturePath = "";
-                if(getIntent().getExtras()!=null && getIntent().getExtras().get("picturePath")!=null){
-                    picturePath = (String) getIntent().getExtras().get("picturePath");
+
+                //if name, weight, and height aren't filled out, throw an alert
+                if (babyName.getText().toString().equals("") || babyHeight.getText().toString().equals("") || babyWeight.getText().toString().equals("")) {
+                    alertDialog.setTitle("Oops!");
+                    alertDialog.setMessage("Please fill out the form completely.");
+                    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertDialog.show();
+                }  else {      // else add child to database
+                   String picturePath = "";
+                    if(getIntent().getExtras()!=null && getIntent().getExtras().get("picturePath")!=null){
+                        picturePath = (String) getIntent().getExtras().get("picturePath");
+                    }
+
+                    /**
+                     * CRUD Operations
+                     * */
+                    // Inserting baby
+                    Log.d("Insert: ", "Inserting ..");
+                    babyDao.addBaby(new Baby(babyName.getText().toString(), babySex.getSelectedItem().toString(),
+                            babyHeight.getText().toString(), babyWeight.getText().toString(), babyDob.getText().toString(), picturePath));
+
+                    // Reading all babies
+                    Log.d("Reading: ", "Reading all babies..");
+                    List<Baby> babies = babyDao.getAllBabies();
+
+                    for (Baby baby : babies) {
+                        String log = "Id: "+baby.getID()+" ,Name: " + baby.getName() + " ,Sex: " + baby.getSex()
+                                + " ,Height: " + baby.getHeight() + " ,Weight: " + baby.getWeight() + " ,DOB: " + baby.getDob();
+
+                        // Writing babies to log
+                        Log.d("Name: ", log);
+                    }
+
+                    babyName.setText("");
+                    babyHeight.setText("");
+                    babyWeight.setText("");
+                    babyDob.setText("");
+
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivityForResult(intent, ADD_CHILD_ACTIVITY_ID);
                 }
-
-                /**
-                 * CRUD Operations
-                 * */
-                // Inserting baby
-                Log.d("Insert: ", "Inserting ..");
-                babyDao.addBaby(new Baby(babyName.getText().toString(), babySex.getSelectedItem().toString(),
-                        babyHeight.getText().toString(), babyWeight.getText().toString(), babyDob.getText().toString(), picturePath));
-
-                // Reading all babies
-                Log.d("Reading: ", "Reading all babies..");
-                List<Baby> babies = babyDao.getAllBabies();
-
-                for (Baby baby : babies) {
-                    String log = "Id: "+baby.getID()+" ,Name: " + baby.getName() + " ,Sex: " + baby.getSex()
-                            + " ,Height: " + baby.getHeight() + " ,Weight: " + baby.getWeight() + " ,DOB: " + baby.getDob();
-
-                    // Writing babies to log
-                    Log.d("Name: ", log);
-                }
-                
-                babyName.setText("");
-                babyHeight.setText("");
-                babyWeight.setText("");
-                babyDob.setText("");
-                
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivityForResult(intent, ADD_CHILD_ACTIVITY_ID);
              }
         });
     }
--- src/main/java/com/feedme/activity/EditChildActivity.java
+++ src/main/java/com/feedme/activity/EditChildActivity.java
@@ -1,8 +1,10 @@
 package com.feedme.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -148,39 +150,56 @@ public class EditChildActivity extends Activity {
             }
         });
 
+        //declare alert dialog
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        
         // button listener for add child button
         addChildButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                //if name, weight, and height aren't filled out, throw an alert
+                if (babyName.getText().toString().equals("") || babyHeight.getText().toString().equals("") || babyWeight.getText().toString().equals("")) {
+                    alertDialog.setTitle("Oops!");
+                    alertDialog.setMessage("Please fill out the form completely.");
+                    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertDialog.show();
+              }  else {      // else add child to database
                 
-                String picturePath = "";
-                if(getIntent().getExtras()!=null && getIntent().getExtras().get("picturePath")!=null){
-                    picturePath = (String) getIntent().getExtras().get("picturePath");
+                    String picturePath = "";
+                    if(getIntent().getExtras()!=null && getIntent().getExtras().get("picturePath")!=null){
+                        picturePath = (String) getIntent().getExtras().get("picturePath");
+                    }
+
+                    /**
+                     * CRUD Operations
+                     * */
+                    // Inserting baby
+                    Log.d("Insert: ", "Inserting ..");
+                    babyDao.updateBaby(new Baby(babyName.getText().toString(), babySex.getSelectedItem().toString(),
+                            babyHeight.getText().toString(), babyWeight.getText().toString(), babyDob.getText().toString(), picturePath), babyId);
+
+                    // Reading all babies
+                    Log.d("Reading: ", "Reading all babies..");
+                    List<Baby> babies = babyDao.getAllBabies();
+
+                    for (Baby baby : babies) {
+                        String log = "Id: "+baby.getID()+" ,Name: " + baby.getName() + " ,Sex: " + baby.getSex()
+                                + " ,Height: " + baby.getHeight() + " ,Weight: " + baby.getWeight() + " ,DOB: " + baby.getDob();
+
+                        // Writing babies to log
+                        Log.d("Name: ", log);
+                    }
+
+
+                    Intent intent = new Intent(v.getContext(), ViewBabyActivity.class);
+                    intent.putExtra("babyName", babyName.getText().toString());
+                    startActivityForResult(intent, 3);
                 }
-
-                /**
-                 * CRUD Operations
-                 * */
-                // Inserting baby
-                Log.d("Insert: ", "Inserting ..");
-                babyDao.updateBaby(new Baby(babyName.getText().toString(), babySex.getSelectedItem().toString(),
-                        babyHeight.getText().toString(), babyWeight.getText().toString(), babyDob.getText().toString(), picturePath), babyId);
-
-                // Reading all babies
-                Log.d("Reading: ", "Reading all babies..");
-                List<Baby> babies = babyDao.getAllBabies();
-
-                for (Baby baby : babies) {
-                    String log = "Id: "+baby.getID()+" ,Name: " + baby.getName() + " ,Sex: " + baby.getSex()
-                            + " ,Height: " + baby.getHeight() + " ,Weight: " + baby.getWeight() + " ,DOB: " + baby.getDob();
-
-                    // Writing babies to log
-                    Log.d("Name: ", log);
-                }
-
-
-                Intent intent = new Intent(v.getContext(), ViewBabyActivity.class);
-                intent.putExtra("babyName", babyName.getText().toString());
-                startActivityForResult(intent, 3);
+                
             }
         });
     }
