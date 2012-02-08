5c73970c79cde21544caa05c2e160a3de2ceb3bf
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-02-08 00:13:29 -0500

    Merged with Steve's code

--- src/main/java/com/feedme/activity/EditDiaperActivity.java
+++ src/main/java/com/feedme/activity/EditDiaperActivity.java
@@ -2,21 +2,34 @@ package com.feedme.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.feedme.R;
+import com.feedme.model.Baby;
+import com.feedme.model.Diaper;
 
 /**
  * User: dayelostraco
  * Date: 2/4/12
  * Time: 1:35 PM
  */
-public class EditDiaperActivity extends DiaperActivity
-{
+public class EditDiaperActivity extends DiaperActivity {
+
+    final Bundle bundle = new Bundle();
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_diaper);
 
         googleAnalyticsTracker.startNewSession(TRACKING_ID, this);
         googleAnalyticsTracker.trackPageView("/Edit-Diaper");
 
-
+        //Baby Data
+        final Baby baby = (Baby) getIntent().getSerializableExtra("baby");
+        bundle.putSerializable("baby", baby);
+        styleActivity(baby.getSex());
+        
+        //Diaper Data
+        final Diaper diaper = (Diaper) getIntent().getSerializableExtra("diaper");
+        System.out.println("diaper = " + diaper.getId());
     }
 }
--- src/main/java/com/feedme/activity/ViewDiapersActivity.java
+++ src/main/java/com/feedme/activity/ViewDiapersActivity.java
@@ -1,10 +1,9 @@
 package com.feedme.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 import com.feedme.R;
 import com.feedme.dao.DiaperDao;
 import com.feedme.model.Baby;
@@ -42,7 +41,90 @@ public class ViewDiapersActivity extends BaseActivity {
         final DiaperDao diaperDao = new DiaperDao(getApplicationContext());
         List<Diaper> allDiapers = diaperDao.getLastDiapersByChild(baby.getId(), 10);
 
-        //TODO: Populate Diaper Table
+        //Populate Table Layout with Diapers
+        TableLayout diaperTable = (TableLayout) findViewById(R.id.diaperTableLayout);
+        if (allDiapers.size() > 0) {
+            int j = 0;
+            for (Diaper diaper : allDiapers) {
+                bundle.putSerializable("diaper", diaper);
+
+                TableRow tableRow = new TableRow(this);  //create new row
+
+                if (j == 0) {
+                    if (baby.getSex().equals("Male")) {
+                        tableRow.setBackgroundColor(0xFFD2EDFC);
+                    } else {
+                        tableRow.setBackgroundColor(0xFFFCD2d2);
+                    }
+                } else {
+                    tableRow.setBackgroundColor(0xFFFFFFFF);
+                }
+                TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                        TableRow.LayoutParams.FILL_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT);
+                tableRow.setLayoutParams(layoutParams);
+                tableRow.setClickable(true);
+                tableRow.setPadding(5, 5, 5, 5);
+
+                LinearLayout linearLayoutHorizontal = new LinearLayout(this);
+
+                ImageView imageView = new ImageView(this);
+                imageView.setMinimumHeight(60);
+                imageView.setMinimumWidth(60);
+                imageView.setBackgroundResource(R.drawable.icon_border);
+                imageView.setImageResource(R.drawable.icon_bug);
+
+                linearLayoutHorizontal.addView(imageView);
+
+                LinearLayout linearLayoutVertical = new LinearLayout(this);
+
+                linearLayoutVertical.setOrientation(LinearLayout.VERTICAL);
+
+                linearLayoutVertical.setPadding(5, 0, 0, 0);
+
+                //print diaper location
+                TextView diaperType = new TextView(this);
+                diaperType.setTextColor(0xFF000000);
+                diaperType.setText("Type - " + diaper.getType());
+                linearLayoutVertical.addView(diaperType);
+
+                //print diaper date
+                TextView diaperDate = new TextView(this);
+                diaperDate.setTextColor(0xFF000000);
+                diaperDate.setText(diaper.getDate());
+                linearLayoutVertical.addView(diaperDate);
+
+                //print diaper time
+                TextView diaperTime = new TextView(this);
+                diaperTime.setTextColor(0xFF000000);
+                diaperTime.setText(diaper.getTime() + " - " + diaper.getTime());
+                linearLayoutVertical.addView(diaperTime);
+
+                linearLayoutHorizontal.addView(linearLayoutVertical);
+
+                tableRow.addView(linearLayoutHorizontal);
+
+                //TODO: The same diaper keeps being passed in the bundle. Fix this.
+                tableRow.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), EditDiaperActivity.class);
+                        intent.putExtras(bundle);
+                        startActivityForResult(intent, VIEW_DIAPER_ACTIVITY_ID);
+                    }
+                });
+
+                /* Add row to TableLayout. */
+                diaperTable.addView(tableRow, new TableLayout.LayoutParams(
+                        TableRow.LayoutParams.FILL_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+
+                j++;
+                if (j == 2) {
+                    j = 0;
+                }
+            }
+
+        }
     }
 
     public void handleButtons(final Bundle bundle)
