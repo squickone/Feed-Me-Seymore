20722b48fd08ddb5687f4afeb4ccc3eb4aec098b
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-02-08 23:28:33 -0500

    Updated BaseObjectComparator to use the dateTime field that is in ISO8601 format.

--- src/main/java/com/feedme/util/BaseObjectComparator.java
+++ src/main/java/com/feedme/util/BaseObjectComparator.java
@@ -1,12 +1,8 @@
 package com.feedme.util;
 
-import android.util.Log;
 import com.feedme.model.BaseObject;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Comparator;
-import java.util.Date;
 
 /**
  * User: dayelostraco
@@ -16,8 +12,7 @@ import java.util.Date;
 public class BaseObjectComparator implements Comparator {
 
     /**
-     * Converts the combination of date and startTime for both objects to a Date and then compares the two. If the date
-     * String cannot be parsed, it falls back to a String comparison.
+     * Basic String comparison of ISO8601 Date Strings
      *
      * @param o
      * @param o1
@@ -26,23 +21,11 @@ public class BaseObjectComparator implements Comparator {
     @Override
     public int compare(Object o, Object o1) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("M-d-yyyy:hh:mm:ss");
-
         BaseObject baseObject1 = (BaseObject) o;
         BaseObject baseObject2 = (BaseObject) o1;
 
-        String dateTime1 = baseObject1.getDate().trim() + ":" + baseObject1.getStartTime();
-        String dateTime2 = baseObject2.getDate().trim() + ":" + baseObject2.getStartTime();
-
-        try {
-            Date date1 = sdf.parse(dateTime1);
-            Date date2 = sdf.parse(dateTime2);
-
-            return date1.compareTo(date2);
-
-        } catch (ParseException e) {
-            Log.d("BaseObjectComparator", "Could not parse BaseObjectDate");
-        }
+        String dateTime1 = baseObject1.getDateTime();
+        String dateTime2 = baseObject2.getDateTime();
 
         return dateTime1.compareToIgnoreCase(dateTime2);
     }
