0b5964ff1ab0f97849604cfc700a6959d43738f8
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-02-09 01:46:58 -0500

    Added Pagenation to the Journal Screen.

--- src/main/java/com/feedme/activity/ViewEntriesActivity.java
+++ src/main/java/com/feedme/activity/ViewEntriesActivity.java
@@ -15,7 +15,9 @@ import com.feedme.model.BaseObject;
 import com.feedme.ui.JournalTable;
 import com.feedme.util.BaseObjectComparator;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,6 +28,10 @@ import java.util.List;
  * Time: 12:29 PM
  */
 public class ViewEntriesActivity extends BaseActivity {
+
+    private static final SimpleDateFormat ANDROID_FORMAT = new SimpleDateFormat("M-d-yyyy ");
+    private static final SimpleDateFormat HEADER_FORMAT = new SimpleDateFormat("MMMMMMMMM, d yyyy");
+    
     private JournalTable journalTable = new JournalTable();
 
     public static final int VIEW_ENTRIES_ACTIVITY_ID = 50;
@@ -38,14 +44,22 @@ public class ViewEntriesActivity extends BaseActivity {
         googleAnalyticsTracker.startNewSession(TRACKING_ID, this);
         googleAnalyticsTracker.trackPageView("/View-Entries");
 
+        Calendar calendar = Calendar.getInstance();
+        if(getIntent().getExtras()!=null && getIntent().getExtras().get("journalDate")!=null){
+            calendar = (Calendar) getIntent().getExtras().get("journalDate");
+        }
+        
+        TextView journalHeader = (TextView) findViewById(R.id.journalHeader);
+        journalHeader.setText(HEADER_FORMAT.format(calendar.getTime()));
+
         final Baby baby = (Baby) getIntent().getSerializableExtra("baby");
-
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("baby", baby);
+        
         styleActivity(baby.getSex());
+        handleButtons(calendar, bundle);
 
-        handleButtons(baby.getId());
-
-
-        List<BaseObject> history = getHistory(baby, getApplicationContext());
+        List<BaseObject> history = getHistory(baby, ANDROID_FORMAT.format(calendar.getTime()),getApplicationContext());
         TableLayout tableLayout = (TableLayout) findViewById(R.id.myTableLayout);
 
         if (history.size() > 0) {
@@ -54,15 +68,37 @@ public class ViewEntriesActivity extends BaseActivity {
 
     }
 
-    public void handleButtons(final int babyId) {
+    public void handleButtons(final Calendar calendar, final Bundle bundle) {
         Button childButton = (Button) findViewById(R.id.childScreen);
         childButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                final BabyDao babyDao = new BabyDao(getApplicationContext());
-
                 Intent intent = new Intent(v.getContext(), ViewBabyActivity.class);
-                intent.putExtra("babyName", babyDao.getBaby(babyId).getName());
+                intent.putExtras(bundle);
+                startActivityForResult(intent, VIEW_ENTRIES_ACTIVITY_ID);
+            }
+        });
+
+        Button previousButton = (Button) findViewById(R.id.previousDay);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                calendar.add(Calendar.DAY_OF_YEAR, -1);
+                Intent intent = new Intent(v.getContext(), ViewEntriesActivity.class);
+                bundle.putSerializable("journalDate", calendar);
+                intent.putExtras(bundle);
+                startActivityForResult(intent, VIEW_ENTRIES_ACTIVITY_ID);
+            }
+        });
+
+        Button nextButton = (Button) findViewById(R.id.nextDay);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                calendar.add(Calendar.DAY_OF_YEAR, 1);
+                Intent intent = new Intent(v.getContext(), ViewEntriesActivity.class);
+                bundle.putSerializable("journalDate", calendar);
+                intent.putExtras(bundle);
                 startActivityForResult(intent, VIEW_ENTRIES_ACTIVITY_ID);
             }
         });
@@ -99,19 +135,20 @@ public class ViewEntriesActivity extends BaseActivity {
      * Returns a List containing Diapers, Naps and Feedings sorted by Date and then StartTime.
      *
      * @param baby
+     * @param date
      * @param context
      * @return
      */
-    private List<BaseObject> getHistory(Baby baby, Context context) {
+    private List<BaseObject> getHistory(Baby baby, String date, Context context) {
 
         JournalDao journalDao = new JournalDao(context);
         DiaperDao diaperDao = new DiaperDao(context);
         NapDao napDao = new NapDao(context);
 
         List<BaseObject> history = new ArrayList<BaseObject>();
-        history.addAll(journalDao.getLastFeedingsByChild(baby.getId(), 20));
-        history.addAll(diaperDao.getLastDiapersByChild(baby.getId(), 20));
-        history.addAll(napDao.getLastNapsByChild(baby.getId(), 20));
+        history.addAll(journalDao.getEntriesForChildByDate(baby.getId(), date));
+        history.addAll(diaperDao.getDiapersForChildByDate(baby.getId(), date));
+        history.addAll(napDao.getNapsForChildByDate(baby.getId(), date));
 
         Collections.sort(history, new BaseObjectComparator());
         Collections.reverse(history);
