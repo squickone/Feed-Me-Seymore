38a808165aec849ad755c46012e0b82bb66e65ac
Stephen Quick <squickone@hotmail.com>
2012-02-09 00:46:55 -0500

    fix broken add child

--- src/main/java/com/feedme/activity/AddChildActivity.java
+++ src/main/java/com/feedme/activity/AddChildActivity.java
@@ -118,10 +118,10 @@ public class AddChildActivity extends ChildActivity
         }
 
         //Take Picture Button
-        takePicture.setOnClickListener(takePictureListener(baby.getId(), ADD_CHILD_ACTIVITY_ID));
+        takePicture.setOnClickListener(takePictureListener(0, ADD_CHILD_ACTIVITY_ID));
 
         //Select Picture Button
-        selectPicture.setOnClickListener(selectPictureListener(baby.getId(), ADD_CHILD_ACTIVITY_ID));
+        selectPicture.setOnClickListener(selectPictureListener(0, ADD_CHILD_ACTIVITY_ID));
 
         //declare alert dialog
         final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
